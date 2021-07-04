6e26b343db5a167e5eff1602810bb7cac8c6858e
Abhijeet <abhijeet8900@gmail.com>
2021-07-05 01:44:59 +0530

    - Dowload pdf on save

--- src/components/note/index.tsx
+++ src/components/note/index.tsx
@@ -2,16 +2,18 @@ import "./note.scss";
 
 import cx from "classnames";
 import ReactQuill from "react-quill";
-import { useEffect, useState } from "react";
 import { useDebounce } from "use-debounce/lib";
+import { useEffect, useRef, useState } from "react";
 
 import Menus from "../menus";
+import { saveAs } from "file-saver";
+import { DeltaStatic, Sources } from "quill";
 import { Font } from "../../constants/fonts";
 import { Theme } from "../../constants/themes";
+import { pdfExporter, RawOrParsedDelta } from "quill-to-pdf";
 import { setLocalNotes, getLocalNotes } from "../../utils/notes";
 import { fontSwitcher, getDefaultFont } from "../../utils/fonts";
 import { themeSwitcher, getDefaultTheme } from "../../utils/theme";
-import { DeltaStatic, Sources } from "quill";
 
 const defaultTheme = getDefaultTheme();
 const defaultFont = getDefaultFont();
@@ -22,6 +24,22 @@ const Note: React.FC = () => {
   const [autoSaveNotes] = useDebounce(text, 5000);
   const [theme, setTheme] = useState<Theme>(defaultTheme);
   const [font, setFont] = useState<Font>(defaultFont);
+
+  let quillRef = useRef<ReactQuill>(null);
+
+  /** Download Notes */
+  const onSave = async () => {
+    const quillDelta: RawOrParsedDelta = quillRef.current
+      ?.getEditor()
+      .getContents() as RawOrParsedDelta;
+    if (typeof quillDelta != "undefined") {
+      const pdfBlob = await pdfExporter.generatePdf(quillDelta);
+      const fileName = `notes-${new Date().toString()}`;
+      saveAs(pdfBlob, fileName);
+    } else {
+      console.error("Error Downloading notes");
+    }
+  };
 
   const menus = [
     {
@@ -41,6 +59,7 @@ const Note: React.FC = () => {
     {
       label: "Save",
       onClick: () => {
+        onSave();
         console.log("Save");
       },
     },
@@ -63,6 +82,7 @@ const Note: React.FC = () => {
   return (
     <div className={cx(`note-container`, theme.className, font.className)}>
       <ReactQuill
+        ref={quillRef}
         className="note"
         placeholder="Type your text...."
         theme="bubble"
